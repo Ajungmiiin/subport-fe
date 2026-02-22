d635cc678455c572ac374e4c9304790f0fe74495
Ajungmiiin <wjdals3249@naver.com>
2026-02-23 01:31:25 +0900

    fix : reminderDaysBeforeEnd 데이터 삭제

--- src/schema/add-subscribe-schema.ts
+++ src/schema/add-subscribe-schema.ts
@@ -2,7 +2,6 @@ import { z } from 'zod';
 
 export const addSubscribeSchema = z.object({
   startDate: z.string(),
-  reminderDaysBeforeEnd: z.number('알림주기는 최소 1일전 입니다.').int().min(1),
   memo: z.string(),
   dutchPay: z.boolean(),
   dutchPayAmount: z.string().nullable(),
