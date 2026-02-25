efc7ffa9c87b5bc5f057186bb0a4b716c19c9ed9
Ajungmiiin <wjdals3249@naver.com>
2026-02-26 02:16:43 +0900

    feat : mutate onSuccess 후속 처리

--- src/hooks/mutations/use-add-subscribe-mutate.ts
+++ src/hooks/mutations/use-add-subscribe-mutate.ts
@@ -1,4 +1,5 @@
 import { addSubscribe } from '@/api/subscribe';
+import { queryClient } from '@/components/providers/query-provider';
 import type { useMutationCallbacks } from '@/types/mutate';
 import { useMutation } from '@tanstack/react-query';
 
@@ -6,6 +7,15 @@ export const useAddSubscribeMutate = (callbacks?: useMutationCallbacks) => {
   return useMutation({
     mutationFn: addSubscribe,
     onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ['member-subscriptions'],
+        predicate: (q) => {
+          const second = q.queryKey[1];
+          return (
+            typeof second === 'object' && second !== null && 'active' in second
+          );
+        },
+      });
       callbacks?.onSuccess?.(data);
     },
   });
