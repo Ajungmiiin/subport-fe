b2665706d7aee26081e07fa155cf0840fa40743e
Ajungmiiin <wjdals3249@naver.com>
2026-02-26 02:16:33 +0900

    feat : mutate onSuccess 후속 처리

--- src/hooks/mutations/use-add-custom-subscribe-mutate.ts
+++ src/hooks/mutations/use-add-custom-subscribe-mutate.ts
@@ -1,11 +1,30 @@
 import { addCustomSubscribe } from '@/api/subscribe';
+import { queryClient } from '@/components/providers/query-provider';
+import { QUERY_KEY } from '@/constants/query-key';
 import type { useMutationCallbacks } from '@/types/mutate';
+import type { subscribeItem } from '@/types/subscribe';
 import { useMutation } from '@tanstack/react-query';
 
-function useAddCustomSubscribeMutate(callbacks?: useMutationCallbacks) {
+function useAddCustomSubscribeMutate(
+  callbacks?: useMutationCallbacks<{ id: number }>,
+) {
   return useMutation({
     mutationFn: addCustomSubscribe,
-    onSuccess: (data) => {
+    onSuccess: (data, variables) => {
+      const prevSubscriptions = queryClient.getQueryData<subscribeItem[]>(
+        QUERY_KEY.subscriptions.all,
+      );
+      if (!prevSubscriptions) return;
+
+      queryClient.setQueryData<subscribeItem[]>(QUERY_KEY.subscriptions.all, [
+        {
+          id: data.id,
+          name: variables.name,
+          logoImageUrl: URL.createObjectURL(variables.image as File),
+          defaultProvided: false,
+        },
+        ...prevSubscriptions,
+      ]);
       callbacks?.onSuccess?.(data);
     },
   });
