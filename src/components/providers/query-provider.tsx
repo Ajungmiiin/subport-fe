164c96b245763ae8837851a97039597b7bf70227
Ajungmiiin <wjdals3249@naver.com>
2026-02-26 02:13:33 +0900

    feat : queryClient export

--- src/components/providers/query-provider.tsx
+++ src/components/providers/query-provider.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient({
+// eslint-disable-next-line react-refresh/only-export-components
+export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: Infinity,
