af4b3ff6dd2b0ae5b3c9f73f16fe6d32fbf1ead9
Ajungmiiin <wjdals3249@naver.com>
2026-03-09 04:23:52 +0900

    fix : success 후속처리 invalidateQueries로 변경

--- src/hooks/mutations/use-add-custom-subscribe-mutate.ts
+++ src/hooks/mutations/use-add-custom-subscribe-mutate.ts
@@ -2,34 +2,15 @@ import { addCustomSubscribe } from '@/api/subscribe';
 import { queryClient } from '@/components/providers/query-provider';
 import { QUERY_KEY } from '@/constants/query-key';
 import type { useMutationCallbacks } from '@/types/mutate';
-import type { subscribeItem } from '@/types/subscribe';
 import { useMutation } from '@tanstack/react-query';
-
-import SubscribeFallbackImage from '@/assets/subscribe-fallback-image.svg';
 
 function useAddCustomSubscribeMutate(
   callbacks?: useMutationCallbacks<{ id: number }>,
 ) {
   return useMutation({
     mutationFn: addCustomSubscribe,
-    onSuccess: (data, variables) => {
-      console.log(data);
-      const prevSubscriptions = queryClient.getQueryData<subscribeItem[]>(
-        QUERY_KEY.subscriptions.all,
-      );
-      if (!prevSubscriptions) return;
-
-      queryClient.setQueryData<subscribeItem[]>(QUERY_KEY.subscriptions.all, [
-        {
-          id: data.id,
-          name: variables.name,
-          logoImageUrl: variables.image
-            ? URL.createObjectURL(variables.image)
-            : SubscribeFallbackImage,
-          defaultProvided: false,
-        },
-        ...prevSubscriptions,
-      ]);
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: QUERY_KEY.subscriptions.all });
       callbacks?.onSuccess?.(data);
     },
   });
--- src/hooks/mutations/use-add-plan-mutate.ts
+++ src/hooks/mutations/use-add-plan-mutate.ts
@@ -1,7 +1,6 @@
 import { addPlan } from '@/api/plan';
 import { QUERY_KEY } from '@/constants/query-key';
 import type { useMutationCallbacks } from '@/types/mutate';
-import type { PlanItem } from '@/types/plan';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 function useAddPlanMutate(callbacks?: useMutationCallbacks) {
@@ -11,17 +10,9 @@ function useAddPlanMutate(callbacks?: useMutationCallbacks) {
     mutationFn: addPlan,
     onSuccess: (data, variables) => {
       callbacks?.onSuccess?.(data);
-      queryClient.setQueryData<PlanItem[]>(
-        QUERY_KEY.plans.list(variables.subscribeId.toString()),
-        (prev) => {
-          if (!prev) throw new Error('멤버십 목록을 불러오지 못했습니다');
-
-          return [
-            ...prev,
-            { ...variables, id: data.id, defaultProvided: false },
-          ];
-        },
-      );
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEY.plans.list(variables.subscribeId.toString()),
+      });
     },
   });
 }
