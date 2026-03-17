3cee65171a5ae04aab9b22cb9ddaf118e25fd1bc
Ajungmiiin <wjdals3249@naver.com>
2026-03-18 00:59:08 +0900

    design : 색상 변경, 마진, 패딩 값 조정

--- src/components/modal/onboarding-bottom-modal.tsx
+++ src/components/modal/onboarding-bottom-modal.tsx
@@ -11,6 +11,7 @@ import OnBoardingSearchIcon from '@/assets/icons/onboarding-search-icon.png';
 import OnBoardingNotiIcon from '@/assets/icons/onboarding-notification-icon.png';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { cn } from '@/lib/utils';
 
 interface OnBoardingBottomModalProps {
   open: boolean;
@@ -25,50 +26,73 @@ function OnBoardingBottomModal({ open }: OnBoardingBottomModalProps) {
 
   return (
     <Drawer open={open}>
-      <DrawerContent className="bg-white data-[vaul-drawer-direction=bottom]:rounded-t-[30px]">
-        <DrawerHeader>
-          <DrawerTitle className="text-xl font-semibold">
-            {onboardingStep === 'exchangeRateNotice' && '환율 적용 기준 안내'}
-            {onboardingStep === 'notificationSetting' && (
+      <DrawerContent className="bg-background-black data-[vaul-drawer-direction=bottom]:rounded-t-[30px]">
+        <div className="pb-10">
+          <div className="flex items-center justify-end gap-1 px-4 py-2.5">
+            <span
+              className={cn(
+                onboardingStep === 'exchangeRateNotice'
+                  ? 'bg-primary'
+                  : 'bg-white',
+                'size-1.5 rounded-full transition-colors',
+              )}
+            ></span>
+            <span
+              className={cn(
+                onboardingStep === 'notificationSetting'
+                  ? 'bg-primary'
+                  : 'bg-white',
+                'size-1.5 rounded-full transition-colors',
+              )}
+            ></span>
+          </div>
+          <DrawerHeader>
+            <DrawerTitle className="text-lg text-white">
+              {onboardingStep === 'exchangeRateNotice' && '환율 적용 기준 안내'}
+              {onboardingStep === 'notificationSetting' && (
+                <>
+                  구독 결제 <br />
+                  이메일 알림을 받아보세요!
+                </>
+              )}
+            </DrawerTitle>
+          </DrawerHeader>
+          <DrawerDescription className="text-sub-font-black mx-auto text-center text-sm/relaxed">
+            {onboardingStep === 'exchangeRateNotice' && (
               <>
-                구독 결제 <br />
-                이메일 알림을 받아보세요!
+                해외 결제 구독은 오후 12시 기준 환율로 계산돼요. <br /> 실제
+                결제 금액과 차이가 있을 수 있어요.
               </>
             )}
-          </DrawerTitle>
-        </DrawerHeader>
-        <DrawerDescription className="text-sub-font-black mx-auto text-sm/relaxed">
-          {onboardingStep === 'exchangeRateNotice' && (
-            <>
-              해외 결제 구독은 오후 12시 기준 환율로 계산돼요. <br /> 실제 결제
-              금액과 차이가 있을 수 있어요.
-            </>
-          )}
 
-          {onboardingStep === 'notificationSetting' && (
-            <>
-              결제 예정일 3일 전에 이메일로 미리 알려드려요. <br />
-              알림 시점은 언제든지 설정에서 변경할 수 있어요.
-            </>
-          )}
-        </DrawerDescription>
-        <img
-          src={
-            onboardingStep === 'exchangeRateNotice'
-              ? OnBoardingSearchIcon
-              : OnBoardingNotiIcon
-          }
-          aria-hidden
-          className="mx-auto my-4 h-auto w-20"
-        />
+            {onboardingStep === 'notificationSetting' && (
+              <>
+                결제 예정일 3일 전에 이메일로 미리 알려드려요. <br />
+                알림 시점은 언제든지 설정에서 변경할 수 있어요.
+              </>
+            )}
+          </DrawerDescription>
+          <img
+            src={
+              onboardingStep === 'exchangeRateNotice'
+                ? OnBoardingSearchIcon
+                : OnBoardingNotiIcon
+            }
+            aria-hidden
+            className="mx-auto mt-8 h-auto w-20"
+          />
+        </div>
         <DrawerFooter>
           {onboardingStep === 'exchangeRateNotice' && (
-            <Button onClick={() => setOnBoardingStep('notificationSetting')}>
+            <Button
+              className="bg-primary-light-active hover:bg-primary-light-hover h-12 rounded-xl text-sm"
+              onClick={() => setOnBoardingStep('notificationSetting')}
+            >
               확인했어요
             </Button>
           )}
           {onboardingStep === 'notificationSetting' && (
-            <div className="flex items-center gap-2">
+            <div className="flex items-center gap-3">
               <Button
                 onClick={() => {
                   navigate('/', { replace: true, state: null });
@@ -78,7 +102,7 @@ function OnBoardingBottomModal({ open }: OnBoardingBottomModalProps) {
                   );
                 }}
                 variant={'cancel'}
-                className="w-full max-w-1/3"
+                className="h-12 w-full max-w-2/5 rounded-xl text-sm"
               >
                 나중에
               </Button>
@@ -90,7 +114,7 @@ function OnBoardingBottomModal({ open }: OnBoardingBottomModalProps) {
                     'consumed',
                   );
                 }}
-                className="flex-1"
+                className="bg-primary-light-active hover:bg-primary-light-hover h-12 flex-1 rounded-xl text-sm"
               >
                 알림 받기
               </Button>
