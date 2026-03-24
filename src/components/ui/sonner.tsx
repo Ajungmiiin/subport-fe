4e7408e2bae532fcc80df0df3d4150b884902eae
Ajungmiiin <wjdals3249@naver.com>
2026-03-24 16:35:13 +0900

    chore : success, error Icon 변경, toast 공통 스타일 추가

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,10 +1,7 @@
-import {
-  CircleCheckIcon,
-  CircleXIcon,
-  InfoIcon,
-  Loader2Icon,
-  TriangleAlertIcon,
-} from 'lucide-react';
+import { InfoIcon, Loader2Icon, TriangleAlertIcon } from 'lucide-react';
+
+import ToastSuccessIcon from '@/assets/icons/toast-success-icon.svg?react';
+import ToastErrorIcon from '@/assets/icons/toast-error-icon.svg?react';
 import { useTheme } from 'next-themes';
 import { Toaster as Sonner, type ToasterProps } from 'sonner';
 
@@ -16,7 +13,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       theme={theme as ToasterProps['theme']}
       className="toaster group"
       offset={{ bottom: 16 }}
-      duration={1000}
+      duration={1500}
       mobileOffset={{ bottom: 16, left: 24, right: 24 }}
       toastOptions={{
         style: {
@@ -24,23 +21,14 @@ const Toaster = ({ ...props }: ToasterProps) => {
           fontSize: '16px',
         },
         classNames: {
-          toast: 'font-sans',
+          toast: 'font-sans !flex !items-center !gap-2',
         },
       }}
       icons={{
-        success: (
-          <CircleCheckIcon
-            className="size-6"
-            strokeWidth={2}
-            stroke={'#ffffff'}
-            fill={'#00B979'}
-          />
-        ),
+        success: <ToastSuccessIcon className="size-6" />,
         info: <InfoIcon className="size-4" />,
         warning: <TriangleAlertIcon className="size-4" />,
-        error: (
-          <CircleXIcon className="fill-d-day-color-7day size-6 stroke-white" />
-        ),
+        error: <ToastErrorIcon className="size-6" />,
         loading: <Loader2Icon className="size-4 animate-spin" />,
       }}
       style={
